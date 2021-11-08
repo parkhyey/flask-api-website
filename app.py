7d277b44079b8449d228f637a9c59387dc24a6af
parkhyey <parkhyey@oregonstate.edu>
2021-11-08 00:56:36 -0800

    Added travel locations for all star signs

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ app.permanent_session_lifetime = datetime.timedelta(days=365)
 
 # Routes
 # Home
+
+
 @app.route("/")
 def root():
     """Render index.html as home page"""
     return render_template("index.html")
+
 
 @app.route("/index.html")
 def index():
@@ -37,15 +40,19 @@ def horoscope():
     return render_template("horoscope.html")
 
 # Travel
-@app.route("/travel.html", methods = ['POST', 'GET'])
-def travel():
+@app.route("/travel.html/<sign>", methods=['POST', 'GET'])
+def travel(sign):
     """Render travel.html"""
-    data = "Zakynthos"
-    response = requests.get("http://flip1.engr.oregonstate.edu:4753/"+data)
+    # select location based on <sign>
+    where = {"Aquarius": "Zakynthos", "Aries": "Bondi Beach", "Cancer": "New Orleans", "Capricorn": "Amalfi Coast", "Gemini": "Lamma Island",
+             "Leo": "Miami", "Libra": "Chefchaouen", "Pisces": "Raja Ampat", "Sagittarius": "Cumbemayo", "Scorpio": "Hawaiʻi Volcanoes National Park", "Taurus": "Costa Rica", "Virgo": "Singapore"}
+
+    response = requests.get(
+        "http://flip1.engr.oregonstate.edu:4753/" + where[sign])
     todos = json.loads(response.text)
     wiki_result = todos["History"]
-    #return redirect(url_for('travel.html', wiki_result=wiki_result))
-    return render_template("travel.html", wiki_result=wiki_result)
+    # return redirect(url_for('travel.html', wiki_result=wiki_result))
+    return render_template("travel.html", wiki_result=wiki_result, where=where[sign], sign=sign)
 
 # All Star Signs
 @app.route("/signs/Aquarius.html")
@@ -53,78 +60,89 @@ def Aquarius():
     """Render Aquarius.html"""
     return render_template("signs/Aquarius.html")
 
+
 @app.route("/signs/Aries.html")
 def Aries():
     """Render Aries.html"""
     return render_template("signs/Aries.html")
+
 
 @app.route("/signs/Cancer.html")
 def Cancer():
     """Render Cancer.html"""
     return render_template("signs/Cancer.html")
 
+
 @app.route("/signs/Capricorn.html")
 def Capricorn():
     """Render Capricorn.html"""
     return render_template("signs/Capricorn.html")
+
 
 @app.route("/signs/Gemini.html")
 def Gemini():
     """Render Gemini.html"""
     return render_template("signs/Gemini.html")
 
+
 @app.route("/signs/Leo.html")
 def Leo():
     """Render Leo.html"""
     return render_template("signs/Leo.html")
+
 
 @app.route("/signs/Libra.html")
 def Libra():
     """Render Libra.html"""
     return render_template("signs/Libra.html")
 
+
 @app.route("/signs/Pisces.html")
 def Pisces():
     """Render Pisces.html"""
     return render_template("signs/Pisces.html")
+
 
 @app.route("/signs/Sagittarius.html")
 def Sagittarius():
     """Render Sagittarius.html"""
     return render_template("signs/Sagittarius.html")
 
+
 @app.route("/signs/Scorpio.html")
 def Scorpio():
     """Render Scorpio.html"""
     return render_template("signs/Scorpio.html")
+
 
 @app.route("/signs/Taurus.html")
 def Taurus():
     """Render Taurus.html"""
     return render_template("signs/Taurus.html")
 
+
 @app.route("/signs/Virgo.html")
 def Virgo():
     """Render Virgo.html"""
     return render_template("signs/Virgo.html")
+
 
 @app.route("/reviews.html")
 def reviews():
     """Render reviews.html"""
     return render_template("reviews.html")
 
-@app.route('/this',methods = ['POST', 'GET'])
-def this():
-    data = "Zakynthos"
-    response = requests.get("http://flip1.engr.oregonstate.edu:4753/"+data)
-    todos = json.loads(response.text)
-    return str(todos)
 
-@app.route("/testing.html")
-def testing():
-    return render_template("testing.html")
+# @app.route('/this', methods=['POST', 'GET'])
+# def this():
+#     data = "Zakynthos"
+#     response = requests.get("http://flip1.engr.oregonstate.edu:4753/"+data)
+#     todos = json.loads(response.text)
+#     return str(todos)
+
 
 # Listener
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33233))
-    app.run(port=port, debug=True) # Use 'python app.py' or 'flask run' to run in terminal
+    # Use 'python app.py' or 'flask run' to run in terminal
+    app.run(port=port, debug=True)
--- static/selections.js
+++ static/selections.js
@@ -1,6 +1,6 @@
 document.getElementById("signshelper").innerHTML =
     '<hr><div id="select">Select your service below</div>'+
-    '<div class="select-class"><a href="../horoscope.html"><div id="select-list">Daily Horoscope</div>'+
-    '<a href="../compatibility.html"><div id="select-list">Astrological Compatibility</div>'+
-    '<a href="../travel.html"><div id="select-list">Destiny Travel</div></div>'
+    '<div class="select-class"><a href="../horoscope.html"><div class="select-list">Daily Horoscope</div>'+
+    '<a href="../compatibility.html"><div class="select-list">Astrological Compatibility</div>'+
+    '<a href="../travel.html"><div class="select-list">Destiny Travel</div></div>'
     ;    
