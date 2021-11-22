9fdf23c99d1f47a69ac91534202bba0cce4fd9d0
parkhyey <parkhyey@oregonstate.edu>
2021-11-21 16:17:42 -0800

    Deleted unused js script

--- static/selections.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("signshelper").innerHTML =
-    '<hr><div id="select">Select your service below</div>'+
-    '<div class="select-class"><a href="../horoscope/"><div class="select-list">Daily Horoscope</div>'+
-    '<a href="../compatibility/"><div class="select-list">Astrological Compatibility</div>'+
-    '<a href="../travel/"><div class="select-list">Destiny Travel</div></div>'
-    ;    
