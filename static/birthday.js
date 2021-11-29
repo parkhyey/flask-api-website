052816e8a24b410acc73c63663c7d6cb87ea568c
parkhyey <parkhyey@oregonstate.edu>
2021-11-28 22:59:17 -0800

    minor refactor

--- static/birthday.js
+++ static/birthday.js
@@ -1,11 +1,11 @@
 // JS function to support dropdown search feature to find star signs
 function birthdayfunc() {
     var select = document.getElementById("birthday");
-    var bir = select.value;
-    if (bir == '') {    
+    var birthday = select.value;
+    if (birthday == '') {    
         document.getElementById("birthday-result").innerHTML = '<span style="color:#FDC286">Select your birthday!</span>';
     } else {
         document.getElementById("birthday-result").innerHTML =
-    'You are ' + '<a href="signs/'+ bir +'" id="bir" >' + bir + '</a>';
+    'You are ' + '<a href="signs/'+ birthday +'" id="bir" >' + birthday + '</a>';
     }
 };
